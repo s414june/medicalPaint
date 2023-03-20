3eb10190a880ebbd3fd486a24c3ad2b2aef03420
etatung <s414june@gmail.com>
2023-03-20 18:39:20 +0800

    初版

---
+++ src/js/modules/layer/raster.js
@@ -0,0 +1,41 @@
+// @ts-nocheck
+import app from "./../../app.js";
+import config from "./../../config.js";
+import Base_layers_class from "./../../core/base-layers.js";
+import Base_state_class from "./../../core/base-state";
+import alertify from "./../../../../node_modules/alertifyjs/build/alertify.min.js";
+
+class Layer_raster_class {
+  constructor() {
+    this.Base_layers = new Base_layers_class();
+	this.Base_state = new Base_state_class();
+  }
+
+  raster() {
+    var canvas = this.Base_layers.convert_layer_to_canvas();
+    var current_layer = config.layer;
+    var current_id = current_layer.id;
+
+    //show
+    var params = {
+      type: "image",
+      name: config.layer.name + " + raster",
+      data: canvas.toDataURL("image/png"),
+      x: parseInt(canvas.dataset.x),
+      y: parseInt(canvas.dataset.y),
+      width: canvas.width,
+      height: canvas.height,
+      opacity: current_layer.opacity,
+    };
+	const last_action = this.Base_state.action_history[this.Base_state.action_history.length - 1];
+    app.State.do_action(
+      new app.Actions.Bundle_action("convert_to_raster", "Convert to Raster", [
+        new app.Actions.Insert_layer_action(params, false),
+        new app.Actions.Delete_layer_action(current_id),
+      ]),
+      { merge_with_history: last_action.action_id }
+    );
+  }
+}
+
+export default Layer_raster_class;
