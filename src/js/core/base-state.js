29f16ab6dbd2e2ccbba4997a8821a74cc8e59e90
etatung <s414june@gmail.com>
2023-03-23 12:45:43 +0800

    debug clear功能 刪除不必要程式碼

--- src/js/core/base-state.js
+++ src/js/core/base-state.js
@@ -150,7 +150,6 @@ class Base_state_class {
 
   async undo_action() {
     if (this.can_undo()) {
-      if (!$("#clear_button").data("can_undo"))
       this.action_history_index--;
       await this.action_history[this.action_history_index].undo();
     } else {
